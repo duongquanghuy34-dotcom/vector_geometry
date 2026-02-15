95e6696105e242f2d893020d706b4aa031beeb97
duongquanghuy34-dotcom <duongquanghuy34@gmail.com>
2026-02-15 09:00:54 +0700

    API paradigm shift

--- geomvector/geomvector.cpp
+++ geomvector/geomvector.cpp
@@ -5,21 +5,21 @@
 #include "geomvectorPriv.hpp"
 
 // Return the dimension of the vector
-size_t VECTOR::geomVectorSize() const{
+size_t VECTOR::Size() const noexcept{
     return vector.size();
 }
 
 // Read the value at index i
-double VECTOR::geomVectorRead(size_t i) const{
-    if(i >= geomVectorSize()){
+double VECTOR::operator()(size_t i) const{
+    if(i >= Size()){
         throw std::invalid_argument("Index out of range");
     }
     return vector[i];
 }
 
 // Modify the value at index i
-void VECTOR::geomVectorChange(size_t i, double a){
-    if(i >= geomVectorSize()){
+void VECTOR::operator()(size_t i, double a){
+    if(i >= Size()){
         throw std::invalid_argument("Index out of range");
     }
     vector[i] = a;
@@ -27,132 +27,135 @@ void VECTOR::geomVectorChange(size_t i, double a){
 
 // Check if two vectors have the same dimension
 bool geomVectorCheckSize(const VECTOR& a, const VECTOR& b){
-    return a.geomVectorSize() == b.geomVectorSize();
+    return a.Size() == b.Size();
 }
 
 // Check if a vector is the zero vector
 bool geomVectorCheckZero(const VECTOR& a){
-    for(size_t i = 0; i < a.geomVectorSize(); i++){
-        if(a.geomVectorRead(i) != 0){
+    for(size_t i = 0; i < a.Size(); i++){
+        if(a(i) != 0){
             return false;   // If any component is non-zero
         }
     }
     return true;
 }
-
 // Add two vectors
-VECTOR geomVectorAdd(const VECTOR& a, const VECTOR& b){
+VECTOR operator+(const VECTOR& a, const VECTOR& b){
     if(!geomVectorCheckSize(a, b)){
         throw std::invalid_argument("Vectors must have the same dimension");
     }
 
-    std::vector<double> d(a.geomVectorSize(), 0);
+    std::vector<double> d(a.Size(), 0);
     VECTOR c(d);
 
-    for(size_t i = 0; i < a.geomVectorSize(); i++){
-        c.geomVectorChange(i, a.geomVectorRead(i) + b.geomVectorRead(i));
+    for(size_t i = 0; i < a.Size(); i++){
+        c(i, a(i) + b(i));
     }
 
     return c;
 }
 
 // Subtract two vectors
-VECTOR geomVectorSub(const VECTOR& a, const VECTOR& b){
+VECTOR operator-(const VECTOR& a, const VECTOR& b){
     if(!geomVectorCheckSize(a, b)){
         throw std::invalid_argument("Vectors must have the same dimension");
     }
 
-    std::vector<double> d(a.geomVectorSize(), 0);
+    std::vector<double> d(a.Size(), 0);
     VECTOR c(d);
 
-    for(size_t i = 0; i < a.geomVectorSize(); i++){
-        c.geomVectorChange(i, a.geomVectorRead(i) - b.geomVectorRead(i));
+    for(size_t i = 0; i < a.Size(); i++){
+        c(i, a(i) - b(i));
     }
 
     return c;
 }
 
 // Compute dot product of two vectors
-double geomVectorDot(const VECTOR& a, const VECTOR& b){
+double operator*(const VECTOR& a, const VECTOR& b){
     if(!geomVectorCheckSize(a, b)){
         throw std::invalid_argument("Vectors must have the same dimension");
     }
 
     double c = 0;
 
-    for(size_t i = 0; i < a.geomVectorSize(); i++){
-        c += a.geomVectorRead(i) * b.geomVectorRead(i);
+    for(size_t i = 0; i < a.Size(); i++){
+        c += a(i) * b(i);
     }
 
     return c;
 }
 
 // Multiply a vector by a scalar
-VECTOR geomVectorMult(const VECTOR& a, double b){
-    std::vector<double> d(a.geomVectorSize(), 0);
+VECTOR operator*(const VECTOR& a, double b){
+    std::vector<double> d(a.Size(), 0);
     VECTOR c(d);
 
-    for(size_t i = 0; i < a.geomVectorSize(); i++){
-        c.geomVectorChange(i, a.geomVectorRead(i) * b);
+    for(size_t i = 0; i < a.Size(); i++){
+        c(i, a(i) * b);
     }
 
     return c;
 }
 
 // Compute the length (magnitude) of the vector
-double VECTOR::geomVectorLength() const{
+double VECTOR::Length() const{
     double c = 0;
 
-    for(size_t i = 0; i < geomVectorSize(); i++){
-        c += std::pow(geomVectorRead(i), 2);
+    for(size_t i = 0; i < Size(); i++){
+        c += std::pow(this->operator()(i), 2);
     }
 
     return std::sqrt(c);
 }
 
 // Normalize a vector
-VECTOR geomVectorNlt(const VECTOR& a){
+VECTOR VectorNormalize(const VECTOR& a){
     if(geomVectorCheckZero(a)){
         throw std::invalid_argument("Cannot normalize the zero vector");
     }
 
-    double length = a.geomVectorLength();
+    double length = a.Length();
 
     std::vector<double> d;
-    for(size_t i = 0; i < a.geomVectorSize(); i++){
-        d.push_back(a.geomVectorRead(i));
+    for(size_t i = 0; i < a.Size(); i++){
+        d.push_back(a(i));
     }
 
     VECTOR e(d);
 
-    for(size_t i = 0; i < a.geomVectorSize(); i++){
-        e.geomVectorChange(i, a.geomVectorRead(i) / length);
+    for(size_t i = 0; i < a.Size(); i++){
+        e(i, a(i) / length);
     }
 
     return e;
 }
 
 // Compute cross product (only for 3D vectors)
-VECTOR geomVectorCross(const VECTOR& a, const VECTOR& b){
-    if(a.geomVectorSize() != 3 || b.geomVectorSize() != 3){
+VECTOR VectorCross(const VECTOR& a, const VECTOR& b){
+    if(a.Size() != 3 || b.Size() != 3){
         throw std::invalid_argument("Cross product is defined only for 3D vectors");
     }
-
     std::vector<double> d(3, 0);
     VECTOR c(d);
-
-    c.geomVectorChange(0,
-        a.geomVectorRead(1) * b.geomVectorRead(2) -
-        a.geomVectorRead(2) * b.geomVectorRead(1));
-
-    c.geomVectorChange(1,
-        a.geomVectorRead(2) * b.geomVectorRead(0) -
-        a.geomVectorRead(0) * b.geomVectorRead(2));
-
-    c.geomVectorChange(2,
-        a.geomVectorRead(0) * b.geomVectorRead(1) -
-        a.geomVectorRead(1) * b.geomVectorRead(0));
-
+    c(0,a(1) * b(2) -a(2) * b(1));
+    c(1,a(2) * b(0) -a(0) * b(2));
+    c(2,a(0) * b(1) -a(1) * b(0));
     return c;
 }
-
+//vector comaprison
+bool operator==(const VECTOR& a, const VECTOR& b){
+    size_t x=a.Size();
+    size_t y=b.Size();
+    if(x != y){
+        throw std::invalid_argument("Vectors must have the same dimension");
+    }
+    else{
+        for(size_t i=0;i<x;i++){
+            if(a(i) != b(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- geomvector/geomvector.hpp
+++ geomvector/geomvector.hpp
@@ -8,41 +8,42 @@ private:
     std::vector<double> vector;
 
 public:
-    VECTOR() = delete;
     // Disable default constructor (no empty vector allowed)
-
+    VECTOR() = delete;
+    
     // Return the dimension of the vector
-    size_t geomVectorSize() const;
+    size_t Size() const noexcept;
 
     // Read the value at index i
-    double geomVectorRead(size_t i) const;
+    double operator()(size_t i) const;
 
     // Modify the value at index i
-    void geomVectorChange(size_t i, double a);
+    void operator()(size_t i, double a);
 
     // Compute the magnitude (length) of the vector
-    double geomVectorLength() const;
+    double Length() const;
 
     // Construct a vector from std::vector<double>
     explicit VECTOR(const std::vector<double>& v) : vector(v) {};
 };
-
 // Add two vectors
-VECTOR geomVectorAdd(const VECTOR& a, const VECTOR& b);
+VECTOR operator+(const VECTOR& a, const VECTOR& b);
 
 // Subtract two vectors
-VECTOR geomVectorSub(const VECTOR& a, const VECTOR& b);
+VECTOR operator-(const VECTOR& a, const VECTOR& b);
 
 // Compute dot product
-double geomVectorDot(const VECTOR& a, const VECTOR& b);
+double operator*(const VECTOR& a, const VECTOR& b);
 
 // Multiply vector by a scalar
-VECTOR geomVectorMult(const VECTOR& a, double b);
+VECTOR operator*(const VECTOR& a, double b);
 
 // Normalize vector
-VECTOR geomVectorNlt(const VECTOR& a);
+VECTOR VectorNormalize(const VECTOR& a);
 
 // Compute cross product (3D only)
-VECTOR geomVectorCross(const VECTOR& a, const VECTOR& b);
+VECTOR VectorCross(const VECTOR& a, const VECTOR& b);
 
+//vector comparison
+bool operator==(const VECTOR& a, const VECTOR& b);
 #endif
